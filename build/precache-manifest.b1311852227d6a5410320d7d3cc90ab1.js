5b4354b0e1e153fb1fbf374c5bd341e8d014b50c
bankir35 <61245298+bankir35@users.noreply.github.com>
2020-04-20 20:31:03 +0300

    Add files via upload

---
+++ build/precache-manifest.b1311852227d6a5410320d7d3cc90ab1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277e588316bf39be8047533f1f97def1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57abb79efb01b65ec967",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "beebe0fb8a41a9b0278a",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "57abb79efb01b65ec967",
+    "url": "/static/js/2.ea2c5a08.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.ea2c5a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beebe0fb8a41a9b0278a",
+    "url": "/static/js/main.57a7387b.chunk.js"
+  },
+  {
+    "revision": "3eb615b4b7f9cb86c138",
+    "url": "/static/js/runtime-main.3077aff4.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
